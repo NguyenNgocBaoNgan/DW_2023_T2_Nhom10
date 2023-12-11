44319351700f1b6ef9fc4173fc07b202cef4dcb8
Nguyễn Vũ Mạnh Hà <20130243@st.hcmuaf.edu.vn>
2023-12-12 02:52:11 +0700

    update

--- loading_to_wh.sql
+++ loading_to_wh.sql
@@ -4,4 +4,4 @@ SQL_UPDATE_DATA_EXPIRED: UPDATE records SET is_expired = ? WHERE date_forcast = 
 
 SQL_INSERT_DATA: INSERT INTO records (province_id, time_record, date_record, time_forcast, date_forcast, temperature, feel_like, description_id, wind_direction_id, wind_speed, humidity, uv_index, cloud_cover, precipitation, accumulation) SELECT province, time_record, date_record, time_forcast, date_forcast, temperature, feel_like, description, wind_direction, wind_speed, humidity, uv_index, cloud_cover, precipitation, accumulation FROM records_staging;
 
-SQL_CHECK_DATA_EXISTS: UPDATE records r INNER JOIN ( SELECT MIN(id) AS id FROM records GROUP BY province_id, date_record, time_record HAVING COUNT(*) > 1 ) dup ON r.id = dup.id SET r.is_expired = 'TRUE';
+SQL_CHECK_DATA_EXISTS: UPDATE records r1 INNER JOIN (  SELECT MAX(id) AS max_id, date_forcast, time_forcast, province_id   FROM records  GROUP BY date_forcast, time_forcast, province_id   HAVING COUNT(*) > 1) r2  ON r1.date_forcast = r2.date_forcast  AND r1.time_forcast = r2.time_forcast AND r1.province_id = r2.province_id SET r1.is_expired = 'TRUE' WHERE r1.id < r2.max_id;
